9bbda3403dd1b5dc812302aac8115a8ed2a7f28b
Leandro Yalet <leandro@iovlabs.org>
2020-04-21 20:58:48 -0300

    chore: added marketplace contracts bundle

---
+++ contracts-bundle/contracts/Imports.sol
@@ -0,0 +1,9 @@
+pragma solidity >=0.4.21 <0.7.0;
+
+import "@rsksmart/rif-marketplace-nfts/contracts/ERC721SimplePlacements.sol";
+import "@rsksmart/rif-marketplace-nfts/contracts/Dummy.sol"; // ERC777Mintable
+
+import "@openzeppelin/contracts/token/ERC20/ERC20Mintable.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721Mintable.sol";
+
+contract Imports {}
