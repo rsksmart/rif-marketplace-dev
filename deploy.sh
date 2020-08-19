df6f312cd4a0147c9de58ba7d70410e08f4994c7
Juraj Piar <juraj@iovlabs.org>
2020-08-19 10:27:30 +0100

    Adds 'all' argument to the deploy script

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,37 @@ if [[ $# -eq 0 ]] ; then
 fi
 
 BASE_DIR=$(pwd)
-    
+
+STORAGE="storage"
+RNS="rns"
+
+SERVICES=($STORAGE $RNS)
+
+if (( $# == 1 && $1 == "all")) ; then
+	for service in "${SERVICES[@]}"
+	do
+		echo "----------------------------"
+		echo "Deploying $service contracts"
+		cd "$BASE_DIR/$service-dev"
+
+		# Migrage deployment
+		npx truffle migrate --network ganache --reset; exitCode=$?
+		if (($exitCode == 0)); then
+			echo "$service contracts deployed"
+		else
+			echo "Migration of $service contracts failed!"
+			exit 1
+		fi
+		echo "---------------------------"
+	done
+
+	cd "$BASE_DIR"
+	echo "Creating config files for $SERVICES"
+	node scripts/createConfig.js "${SERVICES[@]}"
+	echo "Done"
+	exit 0
+fi
+
 for i in "$@"
 do
     cd "$BASE_DIR" 
