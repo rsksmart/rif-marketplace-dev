37f261e5f5dafeab59e02764f0873dcb0836653c
Nazar Duchak <nazar@iovlabs.org>
2021-02-12 13:26:54 +0200

    feat: add triggers contract deployment

--- scripts/createConfig.js
+++ scripts/createConfig.js
@@ -18,6 +18,7 @@ const networkIds = {
 };
 const rnsConfig = {};
 const storageConfig = {};
+const triggersConfig = {};
 var rnsAdminFilePath;
 var storageContractAddress;
 
@@ -50,6 +51,17 @@ networks.forEach(network => {
        storageConfig[network] = JSON.parse(fs.readFileSync(file));
   }
 
+  // Triggers config
+  if (args.includes('triggers')) {
+    const triggersConfigPath = "./triggers-dev/out/";
+    const file = triggersConfigPath + network + ".json";
+    if (fs.existsSync(file))
+      triggersConfig[network] = JSON.parse(fs.readFileSync(file));
+  }
+
+  /**
+   UI CONFIG
+   */
   // Create UI config
   const uiConfig = require("./templates/uiConfig.json");
   const uiOutfile = "./out/ui-config.json";
@@ -66,8 +78,17 @@ networks.forEach(network => {
     uiConfig[network].contractAddresses.storageStaking = storageConfig[network].staking;
     uiConfig[network].services.push("storage");
   }
+  // Triggers
+  if (triggersConfig[network]) {
+    uiConfig[network].contractAddresses.triggersNotificationsManager = triggersConfig[network].notificationsManager;
+    uiConfig[network].contractAddresses.triggersStaking = triggersConfig[network].staking;
+    uiConfig[network].services.push("triggers");
+  }
   fs.writeFileSync(uiOutfile, JSON.stringify(uiConfig, null, 4));
 
+  /**
+    CACHE CONFIG
+  */
   // Create the cache config
   const cacheConfig = require("./templates/cacheConfig.json");
   const cacheOutFile = "./out/cache-" + network + "-config.json";
@@ -101,12 +122,13 @@ networks.forEach(network => {
     const roomsOutFile = "./out/rooms-" + network + ".json";
     storageContractAddress = storageConfig[network].storageManager.toLowerCase();
     roomsConfig.rooms = [
-      networkIds[network] + ":" + storageContractAddress + ":" + "0x90F8bf6A479f320ead074411a4B0e7944Ea8c9C1".toLowerCase(),    
+      networkIds[network] + ":" + storageContractAddress + ":" + "0x90F8bf6A479f320ead074411a4B0e7944Ea8c9C1".toLowerCase(),
       networkIds[network] + ":" + storageContractAddress + ":" + "0xFFcf8FDEE72ac11b5c542428B35EEF5769C409f0".toLowerCase(),
       networkIds[network] + ":" + storageContractAddress + ":" + "0x22d491Bde2303f2f43325b2108D26f1eAbA1e32b".toLowerCase(),
       networkIds[network] + ":" + storageContractAddress + ":" + "0xE11BA2b4D45Eaed5996Cd0823791E0C93114882d".toLowerCase()
     ];
-    fs.writeFileSync(roomsOutFile, JSON.stringify(roomsConfig, null, 4));   
+    fs.writeFileSync(roomsOutFile, JSON.stringify(roomsConfig, null, 4));
   }
+  // TRIGGERS TODO: Create triggers config for cache
   fs.writeFileSync(cacheOutFile, JSON.stringify(cacheConfig, null, 4));
 });
---
+++ triggers-dev/README.md
@@ -0,0 +1,37 @@
+# RIF Marketplace - TRIGGERS dev env
+
+TRIGGERS development environment for RIF Marketplace.
+
+## Run with Ganache
+
+1. Start Ganache, using [docker](https://github.com/rsksmart/rif-marketplace-dev), terminal or the desktop app.
+
+For the terminal: `npm i -g ganache-cli` and `ganache-cli`
+
+2. Migrate the contracts
+
+```
+npx truffle migrate --network ganache
+```
+
+3. The configuration is written in:
+
+```
+./out/ganache.json
+```
+
+## RSK Regtest
+
+1. Run an `rskj` regtest instance. [Here](https://developers.rsk.co/quick-start/step1-install-rsk-local-node/) how to.
+
+2. Migrate the contracts
+
+```
+npx truffle migrate --network regtest
+```
+
+3. The configuration is written in:
+
+```
+./out/regtest.json
+```
