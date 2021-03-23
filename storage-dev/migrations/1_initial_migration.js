ac749eeaafbb861807213060e35ead87f7cb31dc
Nazar Duchak <nazar@iovlabs.org>
2021-03-23 14:06:55 +0200

    feat: adjust deployment script for storage contract

--- storage-dev/migrations/1_initial_migration.js
+++ storage-dev/migrations/1_initial_migration.js
@@ -4,6 +4,9 @@ const Migrations = artifacts.require("Migrations");
 const StorageManager = artifacts.require("StorageManager");
 const Staking = artifacts.require("Staking");
 
+const whiteListedTokens = process.env['WHITE_LISTED_TOKENS'].split(',')
+const whiteListedProviders = process.env['WHITE_LISTED_PROVIDERS'].split(',')
+
 module.exports = async function(deployer, network, accounts) {
   await deployer.deploy(Migrations);
 
@@ -11,59 +14,84 @@ module.exports = async function(deployer, network, accounts) {
 
   console.log("Deploying Storage Manager Contract");
   const marketplaceContract = await deployProxy(StorageManager, [], { deployer, unsafeAllowCustomTypes: true });
-  
-  console.log("Storage Manager - Enabling RBTC Payments");
-  await marketplaceContract.setWhitelistedTokens(
-    "0x0000000000000000000000000000000000000000",
-    true
-  );
-
-  console.log("Storage Manager - Enabling RIF Payments");
-  await marketplaceContract.setWhitelistedTokens(
-    "0x67B5656d60a809915323Bf2C40A8bEF15A152e3e",
-    true
-  );
-
-  console.log("Storage Manager - Whitelisting Provider: " + accounts[0]);
-  await marketplaceContract.setWhitelistedProvider(
-    accounts[0],
-    true
-  );
-
-  console.log("Storage Manager - Whitelisting Provider: " + accounts[1]);
-  await marketplaceContract.setWhitelistedProvider(
-    accounts[1],
-    true
-  );
-
-  console.log("Storage Manager - Whitelisting Provider: " + accounts[2]);
-  await marketplaceContract.setWhitelistedProvider(
-    accounts[2],
-    true
-  );
-
-  console.log("Storage Manager - Whitelisting Provider: " + accounts[3]);
-  await marketplaceContract.setWhitelistedProvider(
-    accounts[3],
-    true
-  );
 
   console.log("Deploying Staking Contract");
   const stakingContract = await deployer.deploy(
-    Staking, marketplaceContract.address
-  );
-  
-  console.log("Staking - Enabling RBTC Payments");
-  await stakingContract.setWhitelistedTokens(
-    "0x0000000000000000000000000000000000000000",
-    true
+      Staking, marketplaceContract.address
   );
 
-  console.log("Staking - Enabling RIF Payments");
-  await stakingContract.setWhitelistedTokens(
-    "0x67B5656d60a809915323Bf2C40A8bEF15A152e3e",
-    true
-  );
+  if (['mainnet', 'testnet'].includes(network)) {
+    for (token of whiteListedTokens) {
+      console.log(`Storage Manager - white listing token ${token}`);
+      await marketplaceContract.setWhitelistedTokens(
+          token,
+          true
+      );
+
+      console.log(`Staking - white listing token ${token}`);
+      await stakingContract.setWhitelistedTokens(
+          token,
+          true
+      );
+    }
+
+    for (provider of whiteListedProviders) {
+      console.log("Storage Manager - Whitelisting Provider: " + provider);
+      await marketplaceContract.setWhitelistedProvider(
+          provider,
+          true
+      );
+    }
+  } else {
+    console.log("Storage Manager - Enabling RBTC Payments");
+    await marketplaceContract.setWhitelistedTokens(
+        "0x0000000000000000000000000000000000000000",
+        true
+    );
+
+    console.log("Storage Manager - Enabling RIF Payments");
+    await marketplaceContract.setWhitelistedTokens(
+        "0x67B5656d60a809915323Bf2C40A8bEF15A152e3e",
+        true
+    );
+
+    console.log("Storage Manager - Whitelisting Provider: " + accounts[0]);
+    await marketplaceContract.setWhitelistedProvider(
+        accounts[0],
+        true
+    );
+
+    console.log("Storage Manager - Whitelisting Provider: " + accounts[1]);
+    await marketplaceContract.setWhitelistedProvider(
+        accounts[1],
+        true
+    );
+
+    console.log("Storage Manager - Whitelisting Provider: " + accounts[2]);
+    await marketplaceContract.setWhitelistedProvider(
+        accounts[2],
+        true
+    );
+
+    console.log("Storage Manager - Whitelisting Provider: " + accounts[3]);
+    await marketplaceContract.setWhitelistedProvider(
+        accounts[3],
+        true
+    );
+
+    console.log("Staking - Enabling RBTC Payments");
+    await stakingContract.setWhitelistedTokens(
+        "0x0000000000000000000000000000000000000000",
+        true
+    );
+
+    console.log("Staking - Enabling RIF Payments");
+    await stakingContract.setWhitelistedTokens(
+        "0x67B5656d60a809915323Bf2C40A8bEF15A152e3e",
+        true
+    );
+
+  }
 
   const configuration = {
     storageManager: marketplaceContract.address,
@@ -82,8 +110,8 @@ module.exports = async function(deployer, network, accounts) {
   const storageConfig = {};
   storageConfig.storageManager = marketplaceContract.address;
   storageConfig.staking = stakingContract.address;
-  
-  
+
+
   await fs.writeFileSync(storageAdminConf, JSON.stringify(storageConfig, null, 4));
 
 };
--- storage-dev/truffle-config.js
+++ storage-dev/truffle-config.js
@@ -18,11 +18,9 @@
  *
  */
 
-// const HDWalletProvider = require('@truffle/hdwallet-provider');
-// const infuraKey = "fj4jll3k.....";
-//
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const mnemonic = process.env['SECRET_MNEMONIC'];
+const nodeUrl = process.env['NODE_URL']
 
 module.exports = {
   plugins: ['truffle-security'],
@@ -48,43 +46,23 @@ module.exports = {
     //  port: 8545,            // Standard Ethereum port (default: none)
     //  network_id: "*",       // Any network (default: none)
     // },
-    regtest: {
-      host: "127.0.0.1", // Localhost (default: none)
-      port: 4444, // Standard Ethereum port (default: none)
-      network_id: "*" // Any network (default: none)
-    },
     ganache: {
       host: "127.0.0.1", // Localhost (default: none)
       port: 8545, // Standard Ethereum port (default: none)
       network_id: "*" // Any network (default: none)
+    },
+    mainnet: {
+      provider: () => new HDWalletProvider(mnemonic, nodeUrl),
+      network_id: 30,
+      // gas: 5500000,
+      production: true
+    },
+    testnet: {
+      provider: () => new HDWalletProvider(mnemonic, nodeUrl),
+      network_id: 31,
+      // gas: 5500000,
+      production: true
     }
-    // Another network with more advanced options...
-    // advanced: {
-      // port: 8777,             // Custom port
-      // network_id: 1342,       // Custom network
-      // gas: 8500000,           // Gas sent with each transaction (default: ~6700000)
-      // gasPrice: 20000000000,  // 20 gwei (in wei) (default: 100 gwei)
-      // from: <address>,        // Account to send txs from (default: accounts[0])
-      // websockets: true        // Enable EventEmitter interface for web3 (default: false)
-    // },
-
-    // Useful for deploying to a public network.
-    // NB: It's important to wrap the provider as a function.
-    // ropsten: {
-      // provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/YOUR-PROJECT-ID`),
-      // network_id: 3,       // Ropsten's id
-      // gas: 5500000,        // Ropsten has a lower block limit than mainnet
-      // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-      // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-      // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-    // },
-
-    // Useful for private networks
-    // private: {
-      // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
-      // network_id: 2111,   // This network is yours, in the cloud.
-      // production: true    // Treats this network as if it was a public net. (default: false)
-    // }
   },
 
   // Set default mocha options here, use special reporters etc.
