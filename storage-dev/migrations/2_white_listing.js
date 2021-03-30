7a28fb1ee000719ef6ed4698122420898f50323e
Artem B <artem.bv@protonmail.com>
2021-03-30 17:45:48 +0300

    fix: add deafult tokens and providers

--- storage-dev/migrations/2_white_listing.js
+++ storage-dev/migrations/2_white_listing.js
@@ -2,6 +2,10 @@ const fs = require("fs");
 const StorageManager = artifacts.require("StorageManager");
 const Staking = artifacts.require("Staking");
 
+const testnetDefaultTokens = ['0x0000000000000000000000000000000000000000', '0x19f64674d8a5b4e652319f5e239efd3bc969a1fe']
+const testnetDefaultProviders = ['0x2Bc6F7B98424Deb932ce6366999258bF22eE1a84', '0x7f20650657001d2F2ae4B9cC602Ec8bA959Fbc4B']
+const mainnetDefaultTokens = ['0x0000000000000000000000000000000000000000', '0x2acc95758f8b5f583470ba265eb685a8f45fc9d5']
+const mainnetDefaultProviders = ['0x4c9619e5c707f3abaf6d5e1be60ba5edda9f486f', '0xca399379b549126aebc8a090849794a66c1d53f3']
 const whiteListedTokens = process.env['WHITE_LISTED_TOKENS'] ? process.env['WHITE_LISTED_TOKENS'].split(',') : []
 const whiteListedProviders = process.env['WHITE_LISTED_PROVIDERS'] ? process.env['WHITE_LISTED_PROVIDERS'].split(',') : []
 
@@ -9,7 +13,17 @@ module.exports = async function(deployer, network, accounts) {
     const marketplaceContract = await StorageManager.deployed();
     const stakingContract = await Staking.deployed()
 
-    if (['mainnet', 'testnet', 'testnet-fork', 'mainnet-fork'].includes(network)) {
+    const isTestnet = ['testnet', 'testnet-fork'].includes(network);
+    const isMainnet = ['mainnet', 'mainnet-fork'].includes(network);
+    if (isTestnet || isMainnet) {
+        if (isTestnet) {
+            whiteListedTokens.push(...testnetDefaultTokens);
+            whiteListedProviders.push(...testnetDefaultProviders);
+        } else if (isMainnet) {
+            whiteListedTokens.push(...mainnetDefaultTokens);
+            whiteListedProviders.push(...mainnetDefaultProviders);
+        }
+        
         for (const token of whiteListedTokens) {
             console.log(`Storage Manager - white listing token ${token}`);
             await marketplaceContract.setWhitelistedTokens(
