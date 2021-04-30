0e5bff374b8e401468d45b2c9c2b4a9f2ccf3967
Artem B <artem.bv@protonmail.com>
2021-04-30 23:11:33 +0300

    feat: add regtest support

--- storage-dev/truffle-config.js
+++ storage-dev/truffle-config.js
@@ -49,6 +49,11 @@ module.exports = {
     //  port: 8545,            // Standard Ethereum port (default: none)
     //  network_id: "*",       // Any network (default: none)
     // },
+    regtest: {
+      host: "127.0.0.1", // Localhost (default: none)
+      port: 4444, // Standard Ethereum port (default: none)
+      network_id: "*" // Any network (default: none)
+    },
     ganache: {
       host: "127.0.0.1", // Localhost (default: none)
       port: 8545, // Standard Ethereum port (default: none)
